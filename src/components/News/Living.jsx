c7983c26f4ccc05771a4779960c0174ee572dd26
Jharna203 <jharnakhatun203@gmail.com>
2025-01-22 08:36:57 +0600

    added living news with reuser news function

--- src/components/News/Living.jsx
+++ src/components/News/Living.jsx
@@ -1,78 +1,75 @@
 import React from "react"
+import ReuseNews from "../../util/ReuseNews/ReuseNews"
 
-const artData = [
+const livingData = [
     {
         'id': '1',
         'topic': 'Style',
         'image': 'living1.webp',
-        'post1': 'Against All Odds, a Vegas Theater Scene With a ‘Sense of the Wild West’',
-        'post2': 'In Minneapolis, a Layer of Hygge Warmth for a Top-Notch Orchestra',
-        'post3': 'At 90, the Ghanaian Highlife Pioneer Ebo Taylor Finds a New Voice'
+        'post1': 'The Ball Gowns of Trump’s New ‘Golden Age’',
+        'post2': 'Fresh Off ‘Severance,’ John Turturro Tries Modeling',
+        'post3': 'Piled On Prints and an ‘Upstate Vibe’'
     },
     {
         'id': '2',
         'topic': 'Food',
         'image': 'living2.webp',
-        'post1': 'The Exile and Rebirth of the South’s Storied ‘Iron Horse’',
-        'post2': 'In an Extravagant New Gallery, Nick Cave Goes Big in Bronze',
-        'post3': 'With Their Lives Upended, They Practiced the Art of Resilience'
+        'post1': 'Restaurant Workers Bring Unique Skills to the L.A. Fire Crisis',
+        'post2': 'As the Power Shifts, D.C. Restaurateurs Are Nervous, and Diplomatic',
+        'post3': 'André Soltner, Famed Chef at New York’s Lutèce, Dies at 92'
     },
     {
         'id': '3',
         'topic': 'Well',
         'image': 'living3.webp',
-        'post1': 'Sex Scenes in Movies Are Back, but They Aren’t Exactly Sexy',
-        'post2': '‘Problemista,’ ‘Stress Positions’ and More Streaming Gems',
-        'post3': '‘Nickel Boys’ Awakens Black Cinema’s Time Revolution'
+        'post1': 'Are Americans Doing Fitness Wrong?',
+        'post2': 'Are Dental X-Rays Safe?',
+        'post3': 'Do LED Face Masks Work?'
     },
     {
         'id': '4',
         'topic': 'The New York Times Magazine',
         'image': 'living4.webp',
-        'post1': 'Trump Returns, and So Does His TV Spectacle',
-        'post2': 'David Lynch, ‘Twin Peaks’ and the American Art of Television',
-        'post3': 'The 13 Greatest ‘S.N.L.’ Commercial Parodies'
+        'post1': 'Curtis Yarvin Says Democracy Is Done. Powerful Conservatives Are Listening.',
+        'post2': 'Can I Go to Church When I Don’t Believe?',
+        'post3': 'Syrians in Turkey Agonize Over a Return Home'
     },
     {
         'id': '5',
         'topic': 'T Magazine',
         'image': 'living5.webp',
-        'post1': 'Garth Hudson, Multifaceted Musician With the Band, Dies at 87',
-        'post2': 'At 90, the Ghanaian Highlife Pioneer Ebo Taylor Finds a New Voice',
-        'post3': 'In Minneapolis, a Layer of Hygge Warmth for a Top-Notch Orchestra'
+        'post1': 'The Movies That Fashion Designers Watch Again and Again',
+        'post2': 'Mexico City’s New Wave of Nostalgic Small Hotels',
+        'post3': 'The Painter Whose Canvases Have Hidden Messages'
     },
     {
         'id': '6',
         'topic': 'Travel',
-        'image': 'living5.webp',
-        'post1': 'Garth Hudson, Multifaceted Musician With the Band, Dies at 87',
-        'post2': 'At 90, the Ghanaian Highlife Pioneer Ebo Taylor Finds a New Voice',
-        'post3': 'In Minneapolis, a Layer of Hygge Warmth for a Top-Notch Orchestra'
+        'image': 'living6.webp',
+        'post1': 'It’s Zinc Bar vs. Barista in a Paris Battle of the Buzz',
+        'post2': 'Never Heard of Yogyakarta? It Might Be the Center of the Universe.',
+        'post3': 'TikTok Fueled Travel Crazes, What Happens Now?'
     },
     {
         'id': '7',
         'topic': 'Love',
-        'image': 'living5.webp',
-        'post1': 'Garth Hudson, Multifaceted Musician With the Band, Dies at 87',
-        'post2': 'At 90, the Ghanaian Highlife Pioneer Ebo Taylor Finds a New Voice',
-        'post3': 'In Minneapolis, a Layer of Hygge Warmth for a Top-Notch Orchestra'
+        'image': 'living7.webp',
+        'post1': 'A Workplace Crush Heats Up Thanks to Burned Cauliflower Pizza',
+        'post2': 'Weddings That Are Out of This World (or at Least Way Out of the Chapel)',
+        'post3': 'You Are Cordially Invited to Our Wedding. But Not to Our Vows.'
     },
     {
         'id': '8',
         'topic': 'Real Estate',
-        'image': 'living5.webp',
-        'post1': 'Garth Hudson, Multifaceted Musician With the Band, Dies at 87',
-        'post2': 'At 90, the Ghanaian Highlife Pioneer Ebo Taylor Finds a New Voice',
-        'post3': 'In Minneapolis, a Layer of Hygge Warmth for a Top-Notch Orchestra'
+        'image': 'living8.webp',
+        'post1': 'Rescuing Pongo, Pearl, Bandit, Tiger and Zipper',
+        'post2': 'Loving an ‘Untouched’ Modernist House, and Then Getting Down to Work',
+        'post3': 'Open Houses in Los Angeles Take on an Eerie Feeling'
     },
 ]
 
 const Living = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return <ReuseNews data={livingData}/>
 };
 
 export default Living;
--- src/components/News/NewsAll.jsx
+++ src/components/News/NewsAll.jsx
@@ -2,17 +2,19 @@ import React from "react"
 import News from "./News";
 import Opinion from "./Opinion";
 import Atr from "./Arts";
+import Living from "./Living";
 
 const NewsAll = () => {
   return (
     <div className="hidden md:block">
       <h2 className="font-sans text-black font-medium py-3 uppercase text-sm">News</h2>
       <News/>
-      <h2 className="font-sans text-black font-medium py-3 uppercase text-sm">Opinion</h2>
+      <h2 className="font-sans text-black font-medium pt-10 pb-3 uppercase text-sm">Opinion</h2>
       <Opinion/>
-      <h2 className="font-sans text-black font-medium py-3 uppercase text-sm">Arts</h2>
+      <h2 className="font-sans text-black font-medium pt-10 pb-3 uppercase text-sm">Arts</h2>
       <Atr/>
-      <h2 className="font-sans text-black font-medium py-3 uppercase text-sm">Living</h2>
+      <h2 className="font-sans text-black font-medium pt-10 pb-3 uppercase text-sm">Living</h2>
+      <Living/>
     </div>
   )
 };
