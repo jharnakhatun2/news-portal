e72066cf1f2eeee5ad563a98fc2fe93f3b4502f5
Jharna203 <jharnakhatun203@gmail.com>
2025-03-10 21:15:21 +0600

    Modify upcomming and comment page

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -64,7 +64,7 @@ const Comment = () => {
       {/* Comment Input Section */}
       <div className="mt-6">
         <textarea
-          className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-100"
           rows="3"
           placeholder="Write your comment..."
           value={newComment}
